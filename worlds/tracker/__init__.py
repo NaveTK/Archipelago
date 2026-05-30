57cf5b64549b8ca3e3435037ad7286203a676d98
NaveTK <timo.koehler.tkh@gmail.com>
2026-05-30 22:27:18 +0200

    Added support for multiple player icons

--- worlds/tracker/__init__.py
+++ worlds/tracker/__init__.py
@@ -164,7 +164,7 @@ class UTMapTabData:
     location_setting_key: str
     """Data storage key used to determine where to place the location indicator"""
 
-    location_icon_coords: Callable[[int, Any], tuple[int,int,str]|None]
+    location_icon_coords: Callable[[int, Any], tuple[int,int,str]|list[tuple[int,int,str]]|None]
     """Function used to convert between the map and the value in data storage into coords (or none to hide it) the return is [x, y, override path string]"""
 
     def __init__(
